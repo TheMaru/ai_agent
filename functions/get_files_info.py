7d6aabc3808963b3c3bb71a8382ab3364789961f
Markus Thiede <mailtothemaru@gmail.com>
2025-10-26 15:34:10 +0100

    implemented first function file returning string folder output added tests to see if text results are usable

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    return_str = ""
+    try:
+        base_dir = os.path.abspath(working_directory)
+        full_path = os.path.abspath(os.path.join(working_directory, directory))
+
+        if not full_path.startswith(base_dir + os.sep) and full_path != base_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(full_path):
+            return f'Error: "{directory}" is not a directory'
+
+        dir_contents = os.listdir(full_path)
+
+        for content in dir_contents:
+            path = os.path.join(full_path, content)
+            return_str += f"- {content}: file_size={os.path.getsize(path)} is_dir={os.path.isdir(path)}\n"
+
+        return return_str
+    except Exception as e:
+        return f"Error: {e}"
